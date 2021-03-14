71dde4f80334309b5369c74c43f5ee92db7cb7a6
Don <dev@solida.io>
2021-03-14 16:30:31 -0600

    Initial commit...

---
+++ migrations/2_deploy.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require("Token");
+const SolidaBank = artifacts.require("SolidaBank");
+
+module.exports = async function(deployer) {
+	//deploy Token
+
+	//assign token into variable to get it's address
+	
+	//pass token address for SolidaBank contract(for future minting)
+
+	//assign SolidaBank contract into variable to get it's address
+
+	//change token's owner/minter from deployer to SolidaBank
+};
